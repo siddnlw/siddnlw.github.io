6cd404d37ea4add7c0ae64b1c63b0303ba9080ff
Siddharth <siddnlw@gmail.com>
2022-09-11 23:45:42 +0530

    added pages for diffrent link from header

--- script.js
+++ script.js
@@ -308,3 +308,29 @@ window.onload = () => {
     return { x: dx, y: dy };
   }
 })();
+const debounce = (fn) => {
+
+  let frame;
+
+  return (...params) => {
+    
+    if (frame) { 
+      cancelAnimationFrame(frame);
+    }
+
+    frame = requestAnimationFrame(() => {
+      
+      fn(...params);
+    });
+
+  } 
+};
+
+
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+}
+
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+
+storeScroll();
